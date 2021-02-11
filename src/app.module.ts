33cfee4697137aaaf2a031347c2bc274754fd705
mike-pinchuk <morning.sunshine.work@gmail.com>
2021-02-11 12:04:33 +0200

    feat: added typedEnv util, connected DB

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { typedEnv } from './utils/typed-env';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot({
+    type: 'mysql',
+    synchronize: true,
+    host: typedEnv.DB_HOST,
+    port: typedEnv.DB_PORT,
+    username: typedEnv.DB_USER,
+    password: typedEnv.DB_PASSWORD,
+    database: typedEnv.DB_NAME,
+    entities: [__dirname + '/../**/*.entity{.ts,.js}']
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }

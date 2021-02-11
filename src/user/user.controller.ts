6279eb6a39e25da13ec3196fd026643d7c57ba7a
mike-pinchuk <morning.sunshine.work@gmail.com>
2021-02-11 18:02:08 +0200

    feat: added get request

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,24 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, NotFoundException, Param, Request } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+import { AuthorizedRequest } from 'src/utils/types';
+import { UserService } from './user.service';
 
-@Controller('user')
-export class UserController {}
+@ApiTags('user')
+@Controller('users')
+export class UserController {
+    constructor(private readonly userService: UserService) { }
+
+    @Get('me')
+    async getProfile(@Request() req: AuthorizedRequest) {
+        return this.userService.getUserbyId(req.user.id)
+    }
+
+    @Get(':id')
+    async getUser(@Param('id') id: string) {
+        const user = await this.userService.getUserbyId(id)
+        if(!user) {
+            throw new NotFoundException('ERROR_USER_NOT_FOUND')
+        }
+        return user
+    }
+}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { UserEntity } from './user.entity';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+    constructor(@InjectRepository(UserEntity) private userRepository: Repository<UserEntity>){}
+
+    async getUserbyId(id: string): Promise<UserEntity | undefined> {
+        return this.userRepository.findOne(id)
+    }
+}

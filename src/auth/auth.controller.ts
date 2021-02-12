907a7bac7e5f1e530845891e8d47334ab916d01d
mike-pinchuk <morning.sunshine.work@gmail.com>
2021-02-12 17:45:08 +0200

    feat(auth): added post request (signup)

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { BadRequestException, Body, Controller, Post } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+import { UserService } from 'src/user/user.service';
+import { AuthService } from './auth.service';
+import { CreateAuthDto } from './dto/auth-user.dto';
 
+@ApiTags('auth')
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+    constructor(private readonly authService: AuthService, private readonly userService: UserService) { }
+
+    @Post('signup')
+    async signUp(@Body() signUpDto: CreateAuthDto) {
+        const existUser = this.userService.findByEmail(signUpDto.email)
+        if (existUser) {
+            throw new BadRequestException('ERROR_USER_WITH_THIS_EMAIL_ARE_EXIST')
+        }
+        const user = await this.userService.createUser(signUpDto.firstName, signUpDto.secondName, signUpDto.age, signUpDto.email, signUpDto.password)
+        return this.authService.createToken(user?.id)
+    }
+}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -4,7 +4,7 @@ import { typedEnv } from 'src/utils/typed-env';
 
 @Injectable()
 export class AuthService {
-    createToken(userId: string) {
+    createToken(userId: string | undefined) {
         const token = jwt.sign({ id: userId }, typedEnv.JWT_SECRET);
         return { token }
     }

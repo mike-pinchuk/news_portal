e38ca785ec38cadf093659e417585cb7d23fc4b2
mike-pinchuk <morning.sunshine.work@gmail.com>
2021-02-12 12:18:01 +0200

    feat(service): class AuthService has  done

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import * as jwt from 'jsonwebtoken';
+import { typedEnv } from 'src/utils/typed-env';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    createToken(userId: string) {
+        const token = jwt.sign({ id: userId }, typedEnv.JWT_SECRET);
+        return { token }
+    }
+}
